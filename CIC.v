e2d9a52b41f414f8e8e6e55bbc023964c27d1164
hi-power <54920534+hi-power@users.noreply.github.com>
2021-11-14 15:09:44 +0800

    CONV

--- CIC.v
+++ CIC.v
@@ -20,20 +20,22 @@ output [2:0] csel;//CONV 運算處理結果寫入/讀取記憶體選擇訊號。
 reg ready;//
 reg busy;
 reg zeropedding;
+reg readtestbench,writetestbench;
 reg [65:0] x,y,k,m;//img位置判斷x從2~65是原圖片
 reg [6:0] x_addr,y_addr;
 reg [11:0] iaddr,iaddr_temp;
 
-reg [8:0] Kernel1_mem,Kernel2_mem;
 
 reg [2:0] csel;
 reg [3:0] 0judge;
 
 reg [19:0]  Block_1,Block_2,Block_3,Block_4, Block_5, Block_6, Block_7, Block_8, Block_9;
+reg [19:0]  Block_1_K1,Block_2_K1,Block_3_K1,Block_4_K1, Block_5_K1, Block_6_K1, Block_7_K1, Block_8_K1, Block_9_K1;
+reg [19:0]  Block_1_K0,Block_2_K0,Block_3_K0,Block_4_K0, Block_5_K0, Block_6_K0, Block_7_K0, Block_8_K0, Block_9_K0;
 
 parameter NSEL = 3'b000, L0K0 = 3'b001,  L0K1 = 3'b010, L1K0 = 3'b011, L1K1 = 3'b100, L2F = 3'b101;//多工器，命名增加可讀性
 parameter UL=4'b0000 ,UR=4'b0001,BL=4'b0010,BR=4'b0011,U=4'b0100,R=4'b0101,L=4'b0110,B=4'b0111,M=4'b1000;
-parameter IDLE = 2'b00, LOAD = 2'b01, WRITE = 2'b11, COMP = 2'b10;//多工器，命名增加可讀性
+parameter IDLE = 2'b00, LOAD = 2'b01, CHPIX = 2'b11, COMP = 2'b10;//多工器，命名增加可讀性
 
 always @(posedge zeropedding) begin//9個zeropedding狀態判斷
     if (x==2 && y==2) begin//iaddr==0 x=2 y=2 iaddr==12'h000
@@ -77,8 +79,7 @@ always @(negedge clk) begin
             Block_2 <= 20'h00000;
             Block_3 <= 20'h00000;
             Block_6 <= 20'h00000;
-            Block_9 <= 20'h00000;
-            
+            Block_9 <= 20'h00000;            
         end
         BL: begin//右上
             Block_1 <= 20'h00000;
@@ -115,7 +116,7 @@ always @(negedge clk) begin
             Block_9 <= 20'h00000;
         end
         default: 
-        reset <= 1;
+        zeropedding <= 0;
     endcase
     zeropedding <= 0;
     end
@@ -124,38 +125,66 @@ always @(negedge clk) begin
 end
 //===========================
 
+always @(*) begin
+    /*Kerne0 CONV*/
+     Block_1_K0 <= Block_1*20'h0A89E;
+     Block_2_K0 <= Block_2*20'h092D5;
+     Block_3_K0 <= Block_3*20'h06D43;
+     Block_4_K0 <= Block_4*20'h0A004;
+     Block_5_K0 <= Block_5*20'hF8F71;
+     Block_6_K0 <= Block_6*20'hF6E54;
+     Block_7_K0 <= Block_7*20'hFA6D7;
+     Block_8_K0 <= Block_8*20'hFC834;
+     Block_9_K0 <= Block_9*20'hFAC19;
+    /*Kernel CONV*/
+     Block_1_K1 <= Block_1*20'hFDB55;
+     Block_2_K1 <= Block_2*20'h02992;
+     Block_3_K1 <= Block_3*20'hFC994;
+     Block_4_K1 <= Block_4*20'h050FD;
+     Block_5_K1 <= Block_5*20'h02F20;
+     Block_6_K1 <= Block_6*20'h0202D;
+     Block_7_K1 <= Block_7*20'h03BD7;
+     Block_8_K1 <= Block_8*20'hFD369;
+     Block_9_K1 <= Block_9*20'h05E68;
+end//k=9時才能啟動
+
+always @(posedge busy) begin
+    ready <=0;
+end
+always @(negedge busy) begin
+    ready <=1;
+end
 
 always @(posedge clk or posedge reset ) begin
     if (reset) begin
         csel <= NSEL; state <= IDLE; ready <= 0; busy <= 0; crd <= 0; cwr <= 0; x <= 2; y <= 2; k <= 1;
+        zeropedding <= 0;
     end
     else begin
         case (state)
 	IDLE : begin //
         csel <= NSEL;
 		state <=  LOAD;  
-		ready <= 1; 
+		ready <= 1; busy <= 0;
 		x <= 2; y <= 2; k <= 1;
 		crd <= 0; cwr <= 0;
 		end
 	LOAD : begin //要求testfixture給資料
 		crd <= 0; cwr <= 0;
-		ready <= 1;
         busy <= 1;
 		k <= k + 1;
-		if (k == 9) begin state <= COMP;   ready <= 1;end//寫入第8狀態時k就會變成9，下一個狀態就會寫入完畢、所以進入COMP狀態
+		if (k == 9) begin state <= COMP;  end//寫入第8狀態時k就會變成9，下一個狀態就會寫入完畢、所以進入COMP狀態
 		else state <= LOAD;
 		end
-	COMP : begin //過渡狀態，關掉向testfixture要資料的權限，開始進入寫入狀態
-		state <= WRITE;
-		ready <= 0;
-        busy <= 0;
+	COMP : begin //關掉向testfixture讀寫資料的權限，算下一個像素
+		state <= CHPIX;
+        cwr <= 1;
 		end
-	WRITE : begin //一列一列讀，令x為列y為行，可稱為一列一列讀
+	CHPIX : begin //一列一列讀，令x為列y為行，可稱為一列一列讀
 		
-		if ((x==65)&(y==65)) begin state <= IDLE; finish <= 1; ready <= 0; end //若已經讀到x、y皆為7(8個資料)表示寫入完成，讓finish=1
-		else if (x == 65) begin y <= y + 1; x <= 2; k<= 1; state<= LOAD; ready <= 0;end //若一列x讀完，換行繼續讀
-		else begin x <= x + 1; k<= 1; state<= LOAD; ready <= 0;end//讀該列的x
+		if ((x==65)&(y==65)) begin state <= IDLE;  end //整張圖片(12byte)寫入完成
+		else if (x == 65) begin y <= y + 1; x <= 2; k<= 1; state<= LOAD; end //若一列x讀完，換行繼續讀
+		else begin x <= x + 1; k<= 1; state <= LOAD; end//讀該列的x
 		end
 	endcase
     end
@@ -168,7 +197,7 @@ always @(posedge clk) begin
                 crd <= 0; cwr <= 0;
            end
            L0K0 : begin//寫入/讀取 Layer 0，Kernel 0 執行 Convolutional 的結果。
-
+                
                
            end
            L0K1 : begin//寫入/讀取 Layer 0，Kernel 1 執行 Convolutional 的結果。
